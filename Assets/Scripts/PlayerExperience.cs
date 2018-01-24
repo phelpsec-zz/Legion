80239da7e6680436abbb6a42c50c215618fda3d9
phelpsec <phelpsec@gmail.com>
2018-01-24 00:05:23 -0500

    Add player experience bar

---
+++ Assets/Scripts/PlayerExperience.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerExperience : MonoBehaviour
+{
+    private float currentExperience = 20;
+
+    private GameObject experienceBar;
+
+    void Awake()
+    {
+        experienceBar = GameObject.Find("Experience");
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float experiencePercentage = currentExperience / 100;
+        experienceBar.transform.localScale = new Vector3(experiencePercentage, experienceBar.transform.localScale.y, experienceBar.transform.localScale.z);
+    }
+
+}
