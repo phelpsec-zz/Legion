c33c8a4487e8268b4e4c49293ba9fa3b9627c330
phelpsec <phelpsec@gmail.com>
2018-01-31 13:54:55 -0500

    Add Ground Stomp spell

---
+++ Assets/Scripts/SpellsGroundStomp.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellsGroundStomp : Spells
+{
+    private SpellsGroundStomp(string spellName, bool isActive, int resourceCost, int resourceGenerate) 
+        : base(spellName, isActive, resourceCost, resourceGenerate) { }
+
+    void Start()
+    {
+        Damage = 50;
+        Range = 20;
+
+        Collider[] enemyList = Physics.OverlapSphere(transform.position, Range);
+
+        foreach (Collider enemy in enemyList)
+        {      
+            if (enemy.tag == "Enemy")
+            {
+                EnemyHealth enemyHealth = enemy.gameObject.GetComponent<EnemyHealth>();
+                enemyHealth.TakeDamage(Damage);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
